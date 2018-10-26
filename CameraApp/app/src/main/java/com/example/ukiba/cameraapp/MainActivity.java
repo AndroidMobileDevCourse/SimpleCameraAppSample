2f2f1c692dd5c69dbe9b94dca84fc98e8d547ce7
Baubek <unreal-kz@users.noreply.github.com>
2018-10-26 21:31:42 +0600

    Simple Camera App sample

---
+++ CameraApp/app/src/main/java/com/example/ukiba/cameraapp/MainActivity.java
@@ -0,0 +1,58 @@
+package com.example.ukiba.cameraapp;
+
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.media.Image;
+import android.os.Bundle;
+import android.provider.MediaStore;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.design.widget.BottomNavigationView;
+import android.support.v7.app.AppCompatActivity;
+import android.view.MenuItem;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    private TextView mTextMessage;
+    private ImageView imgView;
+
+    private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
+            = new BottomNavigationView.OnNavigationItemSelectedListener() {
+
+        @Override
+        public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+            switch (item.getItemId()) {
+                case R.id.navigation_home:
+//                    mTextMessage.setText(R.string.title_home);
+                    return true;
+                case R.id.navigation_camera:
+//                    mTextMessage.setText(R.string.title_dashboard);
+                    startActivityForResult(new Intent(MediaStore.ACTION_IMAGE_CAPTURE),0);
+                    return true;
+                case R.id.navigation_notifications:
+//                    mTextMessage.setText(R.string.title_notifications);
+                    return true;
+            }
+            return false;
+        }
+    };
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main_layout);
+
+        imgView = (ImageView) findViewById(R.id.imgView_2);
+        BottomNavigationView navigation = (BottomNavigationView) findViewById(R.id.navigation_2);
+        navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        Bitmap bitmap = (Bitmap) data.getExtras().get("data");
+        imgView.setImageBitmap(bitmap);
+    }
+}
